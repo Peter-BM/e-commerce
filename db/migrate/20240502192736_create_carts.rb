de14986fd191011453f6bbc65a1952dec379f9c7
Pedro Meneghini <pedrobobella@gmail.com>
2025-09-05 11:24:07 -0300

    feat[cart]: adiciona novos campos à tabela carts.

--- db/migrate/20240502192736_create_carts.rb
+++ db/migrate/20240502192736_create_carts.rb
@@ -2,6 +2,9 @@ class CreateCarts < ActiveRecord::Migration[7.1]
   def change
     create_table :carts do |t|
       t.decimal :total_price, precision: 17, scale: 2
+      t.string :status
+      t.datetime :last_interation_at
+      t.datetime :abandoned_at
 
       t.timestamps
     end
