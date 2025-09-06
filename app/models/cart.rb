4b026cb5696b0366d0aa2efd56dcdecbe914e45a
Pedro Meneghini <pedrobobella@gmail.com>
2025-09-06 17:55:55 -0300

    feat[cart]: adiciona funcao para calcular valor total do carrinho

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,5 +2,11 @@ class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :products, through: :cart_items
 
+  before_save :sum_total_price
+
   # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
+
+  def sum_total_price
+    self.total_price = self.cart_items.sum(&:total_price)
+  end
 end
