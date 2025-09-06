d73966836f22b7346cf97d3c726d3b086d10487a
Pedro Meneghini <pedrobobella@gmail.com>
2025-09-06 14:56:51 -0300

    refactor[products]: renomeia campo prince para unit_price nos produtos

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   has_many :carts, through: :cart_items
 
-  validates_presence_of :name, :price
-  validates_numericality_of :price, greater_than_or_equal_to: 0
+  validates_presence_of :name, :unit_price
+  validates_numericality_of :unit_price, greater_than_or_equal_to: 0
 end
