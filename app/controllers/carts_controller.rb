b1321530574a50cdacc0018fa12ebab9a4e3ce85
Pedro Meneghini <pedrobobella@gmail.com>
2025-09-06 13:24:27 -0300

    feat[cart]: cria rota e action para criar carrinhos na aplicação

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,26 @@
 class CartsController < ApplicationController
-  ## TODO Escreva a lógica dos carrinhos aqui
+  
+  def create
+    @cart = find_or_create_cart
+    render json: @cart.as_json, status: :created
+  end 
+
+  private 
+
+  def cart_params
+    params.require(:cart).permit(:total_price, :status, :last_interation_at, :abandoned_at) 
+  end
+
+  def find_or_create_cart
+    current_cart = session[:cart_id]
+
+    if current_cart.present?
+      @cart ||= Cart.find_by(id: session[:cart_id])
+    else
+      @cart = Cart.create!(last_interation_at: Time.current,
+                           status: 'active')
+      session[:cart_id] = @cart.id
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   resources :products
+  
+  post "cart" => "carts#create"
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "rails/health#show"
