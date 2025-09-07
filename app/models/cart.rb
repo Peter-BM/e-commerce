4dae548304b7a0d59e132d53d7e3706442bc49d0
Pedro Meneghini <pedrobobella@gmail.com>
2025-09-06 22:20:02 -0300

    feat[cart]: adiciona scopes para verificar carrinhos abandonados e invativos.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,7 +4,8 @@ class Cart < ApplicationRecord
 
   before_save :sum_total_price
 
-  # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
+  scope :inactive_since,  -> (time) { where(status: "active").where("last_interation_at < ?", time)}
+  scope :abandoned_since, -> (time) { where(status: "abandoned").where("abandoned_at < ?", time) }
 
   def sum_total_price
     self.total_price = self.cart_items.sum(&:total_price)
